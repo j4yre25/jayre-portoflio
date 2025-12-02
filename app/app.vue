29ebe3c492942d016207fa522211573c458590ac
j4yre25 <reillojan@gmail.com>
2025-12-02 20:18:12 +0800

    preview image

--- app/app.vue
+++ app/app.vue
@@ -19,8 +19,8 @@ useHead({
 
 useSeoMeta({
   titleTemplate: '%s - Jayre Portfolio',
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
+  ogImage: 'img/jayre-portfolio.png',
+  twitterImage: 'img/jayre-portfolio.png',
   twitterCard: 'summary_large_image'
 })
 
